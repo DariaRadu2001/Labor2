9e4bce618fd96f5fd50e6c12960332e5bcf8ab7a
DariaRadu2001 <dariaradu20@yahoo.com>
2021-10-18 21:37:16 +0300

    main

--- src/Labor2/Main.java
+++ src/Labor2/Main.java
@@ -17,29 +17,19 @@ public class Main {
         Basketball basketball = new Basketball();
         Hindernislauf hindernislauf = new Hindernislauf();
         Hochsprung hochsprung = new Hochsprung();
+        Sport sport = new Hochsprung();
 
-        List<Sport> list_ana = new ArrayList<Sport>();
-        list_ana.add(fussball);
-        list_ana.add(hochsprung);
-        Benutzer ana = new Benutzer("Ana", "Pop", list_ana);
+        List<Sport> list_daria = new ArrayList<Sport>();
+        list_daria.add(fussball);
+        list_daria.add(hochsprung);
+        list_daria.add(basketball);
+        list_daria.add(hindernislauf);
+        list_daria.add(fussball);
+        list_daria.add(sport);
+        Benutzer daria = new Benutzer("Daria", "Radu", list_daria);
 
-        List<Sport> list_mihai = new ArrayList<Sport>();
-        list_mihai.add(fussball);
-        list_mihai.add(basketball);
-        list_mihai.add(hochsprung);
-        Benutzer mihai = new Benutzer("Mihai", "Radu", list_mihai);
-
-        Benutzer iana = new Benutzer("Iana", "Costel", new ArrayList<Sport>(){});
-
-        System.out.println("Mihais Durchschnitt = " + mihai.kalkuliereDurchschnittszeit());
-        System.out.println("Mihais total Zeit = " + mihai.kalkuliereZeit());
-
-        System.out.println("Anas Durchschnitt = " + ana.kalkuliereDurchschnittszeit());
-        System.out.println("Anas total Zeit = " + ana.kalkuliereZeit());
-
-        System.out.println("Iana are Durchschnitt = " + iana.kalkuliereDurchschnittszeit());
-        System.out.println("Iana are Zeit = " + iana.kalkuliereZeit());
-
+        System.out.println("Darias Durchschnitt = " + daria.kalkuliereDurchschnittszeit());
+        System.out.println("Darias total Zeit = " + daria.kalkuliereZeit());
 
     }
 }
